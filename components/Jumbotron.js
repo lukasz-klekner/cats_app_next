6f12e07d5d692b527b8a1357923128c3c89965c9
Łukasz Klekner <lukaszklekner@gmail.com>
2022-02-05 13:08:32 +0100

    adds Jumbotron component

---
+++ components/Jumbotron.js
@@ -0,0 +1,15 @@
+const Jumbotron = () => (
+  <div className='jumbotron jumbotron-fluid mt-5'>
+    <div className='container'>
+      <h1 className='display-4'>Cats world</h1>
+      <p className='lead'>
+        Find your new best friend today! Adopt a wonderful cat :-)
+      </p>
+      <button type='button' className='btn btn-primary'>
+        Start looking
+      </button>
+    </div>
+  </div>
+)
+
+export default Jumbotron
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+import Jumbotron from '../components/Jumbotron'
 import Nav from '../components/Nav'
 
 export default function Home() {
   return (
     <main>
       <Nav />
+      <Jumbotron />
     </main>
   )
 }
